2ce773cd9939fb7966e8d0bea3f8fa78e5e394ff
babubin <gleblutsko@gmail.com>
2023-08-18 10:45:59 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import Notiflix from 'notiflix';
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-let currentPage = 10;
+let currentPage = 1;
 loadMoreBtn.hidden = true;
 
 form.addEventListener('submit', sumbitForm);
